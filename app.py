34bb9c22b93b3cd47794ff059456441f83fe23ee
ohseungmin <ohmin2886@gmail.com>
2024-02-21 13:20:12 +0900

    python은 request를 모듈로 불러와서 사용해야한다. 그리고 POST 요청으로 log.json에 데이터 저장

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def get_logs():
-    try:
-        with open('log.json', 'r', encoding='utf-8') as f:
-            logs = f.readlines()
-    except FileNotFoundError:
-        return jsonify({'error': 'Log file not found'}), 404
+def index():
+    #반환으로 template에 index.html파일을 지정
+    return render_template('index.html')
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.get('text_bar')
 
-    return jsonify({'logs': logs})
+    with open('log.json', 'a', encoding='utf-8') as f:
+        f.write(data + '\n')
+
+    return jsonify({'message': 'Data received and logged successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
